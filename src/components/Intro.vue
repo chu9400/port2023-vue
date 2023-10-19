db37d1636a9965d793c93f2c914736082e0e8577
Hanul <chu9400@naver.com>
2023-10-19 20:10:12 +0900

    😘 인트로 완료

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,3 +1,175 @@
+<script setup>
+    import { introText } from "../constants";
+</script>
+
 <template>
-    intro
+    <section id="intro">
+        <div class="intro__inner">
+            <h2 class="intro__title">
+                {{ introText.title }}
+            </h2>
+            <div class="intro__lines" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+            <div class="intro__text">
+                <div class="text">
+                    <div>{{ introText.desc[0] }}</div>
+                    <div>{{ introText.desc[1] }}</div>
+                    <div>{{ introText.desc[2] }}</div>
+                </div>
+                <div class="img">
+                    <img src="@/assets/img/about.jpg" alt="내 사진">
+                </div>
+            </div>
+            <div class="intro__lines bottom" aria-hidden="true">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+        </div>
+    </section>
 </template>
+
+<style lang="scss" scoped>
+#intro{
+    height: 100vh;
+}
+
+.intro__inner {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-end;
+    flex-direction: column;
+    padding: 16px;
+
+    @media (max-width:800px){
+        justify-content: center;
+    }
+
+    .intro__title {
+        font-size: 10vw;
+        text-transform: uppercase;
+        line-height: 1;
+        font-weight: 800;
+        white-space: nowrap; /* 개행 금지 */
+        text-indent: -0.5vw; /* 들여쓰기 */
+        letter-spacing: -0.3vw; /* 글자 사이 공백 줄이기 */
+
+        @media(max-width:320px) {
+            display: none;
+        }
+    }
+
+    .intro__text{
+        width: 100%;
+        height: 40vh;
+        background-color: var(--black);
+        color: var(--white);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        position: relative;
+
+        .text {
+            font-size: 3vw;
+            text-transform: uppercase;
+            line-height: 1;
+            font-weight: 900;
+            text-decoration: underline;
+            /* position: relative; */
+            text-align: center;
+            z-index: 100;
+            transition: all 0.3s;
+            cursor: all-scroll;
+
+            @media(max-width:800px) {
+                font-size: 24px;
+            }
+        }
+
+        .img {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            width: 30vh;
+            height: 30vh;
+        
+            border-radius: 50%; /* 사진 동그랗게하기 */
+            overflow: hidden; /* 사진 동그랗게하기 */
+            filter: grayscale(100%); /* 사진 회색으로 */
+            transition: all 0.3s;
+            
+            @media(max-width:800px) {
+                width: 20vh;
+                height: 20vh;
+            }
+
+        }
+
+        &:hover .text {
+            opacity: 0;
+        }
+
+        &:hover .img {
+            filter: grayscale(0);
+        }
+    }
+    .intro__lines{
+        width: 100%;
+
+        .line{
+            display: block;
+            width: 100%;
+            background-color: var(--black);
+            height: 1px;
+            margin-bottom: 0.5vw;
+
+            &:nth-child(1){ height: 1px;}
+            &:nth-child(2){ height: 2px;}
+            &:nth-child(3){ height: 5px;}
+            &:nth-child(4){ height: 9px;}
+            &:nth-child(5){ height: 13px;}
+            &:nth-child(6){ height: 17px;}
+            &:nth-child(7){ height: 25px;}
+        }
+
+        &.bottom { /* 모바일 화면에 표시 */
+            .line{
+                margin-top: 0.5vw;
+                margin-bottom: 0;
+                display: none;
+
+                @media (max-width:800px){
+                    display: block;
+                }
+
+                &:nth-child(1){ height: 25px;}
+                &:nth-child(2){ height: 17px;}
+                &:nth-child(3){ height: 13px;}
+                &:nth-child(4){ height: 9px;}
+                &:nth-child(5){ height: 5px;}
+                &:nth-child(6){ height: 2px;}
+                &:nth-child(7){ height: 1px;}
+
+            }
+            
+        }
+    }
+    
+    
+}
+</style>
